4cf5a07b5b0bd8f1922c33e8a16ead017f53086c
Kuchizu <70284260+Kuchizu@users.noreply.github.com>
2020-10-13 13:03:48 +0500

    Update afk.py

--- afk.py
+++ afk.py
@@ -6,7 +6,7 @@ import time
 
 from telethon import types
 
-logger = logging.getLogger(name)
+logger = logging.getLogger(__name__)
 
 
 @loader.tds
@@ -15,32 +15,32 @@ class AFKMod(loader.Module):
 
     strings = {
         "name": "AFK",
-        "gone": "<b>😭 Мой сенпай в афк 😭</b>",
-        "back": "<b>😊 Мой сейпай больше не в афк 😊</b>",
-        "afk": "<b>🥺 Мой сенпай сейчас в афк (уже {}). 🥺</b>",
-        "afk_reason": "<b>🥺 Мой сенпай сейчас в афк (уже {}). 🥺\nНо он просил передать:</b> <i>{}</i>",
+        "gone": "<b>Извините, но хозяин не в сети! 🥺</b>",
+        "back": "<b>Мой милый хозяин вернулся! 😊</b>",
+        "afk": "<b>Моего хозяина уже нет {}.\nОн не разрешает общаться с незнакомыми! 🥺</b>",
+        "afk_reason": "<b>Моего хозяина уже нет {}.\nОн не разрешает общаться с незнакомыми! 🥺 Но попросил обняв передать:</b> <i>{}</i>",
     }
 
     async def client_ready(self, client, db):
         self._db = db
         self._me = await client.get_me()
 
-    async def afk2cmd(self, message):
+    async def afkcmd(self, message):
         """.afk [message]"""
         if utils.get_args_raw(message):
-            self._db.set(name, "afk", utils.get_args_raw(message))
+            self._db.set(__name__, "afk", utils.get_args_raw(message))
         else:
-            self._db.set(name, "afk", True)
-        self._db.set(name, "gone", time.time())
-        self._db.set(name, "ratelimit", [])
+            self._db.set(__name__, "afk", True)
+        self._db.set(__name__, "gone", time.time())
+        self._db.set(__name__, "ratelimit", [])
         await self.allmodules.log("afk", data=utils.get_args_raw(message) or None)
         await utils.answer(message, self.strings("gone", message))
 
-    async def unafk2cmd(self, message):
+    async def unafkcmd(self, message):
         """Remove the AFK status"""
-        self._db.set(name, "afk", False)
-        self._db.set(name, "gone", None)
-        self._db.set(name, "ratelimit", [])
+        self._db.set(__name__, "afk", False)
+        self._db.set(__name__, "gone", None)
+        self._db.set(__name__, "ratelimit", [])
         await self.allmodules.log("unafk")
         await utils.answer(message, self.strings("back", message))
 
@@ -52,11 +52,11 @@ class AFKMod(loader.Module):
             if not afk_state:
                 return
             logger.debug("tagged!")
-            ratelimit = self._db.get(name, "ratelimit", [])
+            ratelimit = self._db.get(__name__, "ratelimit", [])
             if utils.get_chat_id(message) in ratelimit:
                 return
             else:
-                self._db.setdefault(name, {}).setdefault("ratelimit", []).append(
+                self._db.setdefault(__name__, {}).setdefault("ratelimit", []).append(
                     utils.get_chat_id(message)
                 )
                 self._db.save()
@@ -68,7 +68,7 @@ class AFKMod(loader.Module):
                 return
             now = datetime.datetime.now().replace(microsecond=0)
             gone = datetime.datetime.fromtimestamp(
-                self._db.get(name, "gone")
+                self._db.get(__name__, "gone")
             ).replace(microsecond=0)
             diff = now - gone
             if afk_state is True:
@@ -78,4 +78,4 @@ class AFKMod(loader.Module):
             await utils.answer(message, ret, reply_to=message)
 
     def get_afk(self):
-        return self._db.get(name, "afk", False)
+        return self._db.get(__name__, "afk", False)
